74f4302f8ff2e61bd41f0313b41b70e006ae2042
Fisayo <fasuyifisayo@gmail.com>
2022-10-13 15:34:06 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+
+/**
+ * main - Entry point function
+ *
+ *
+ * Return: Returns o
+ */
+
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
