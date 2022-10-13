c3da4ea682787c06acc7025d3a2fbab02c54eee5
Fisayo <fasuyifisayo@gmail.com>
2022-10-13 16:34:38 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -15,8 +15,7 @@ int main(void)
 
 {
 
-fwrite(STDOUT_FILENO, "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is usefu - Dora Korpar, 2015-10-19\n", 58);
 
 return (1);
 
