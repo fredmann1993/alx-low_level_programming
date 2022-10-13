d6440348cdc561be736e51f02afca266cf89941d
Fisayo <fasuyifisayo@gmail.com>
2022-10-13 16:47:21 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 
 
@@ -13,7 +13,9 @@ int main(void)
 
 {
 
-write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
 
 return (1);
 
