80b585c5c96019cead08014985807f524fd38275
Fisayo <fasuyifisayo@gmail.com>
2022-10-13 15:41:02 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Function for the program
+ *
+ * Return: Returns 0
+ *
+ */
+
+
+int main(void)
+{
+   printf("with proper grammar, but the outcome is a piece of art,\n");
+   return (0);
+}
