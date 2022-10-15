d69f809bed270719f78b4dfb6c118e44691bbc80
Fisayo <fasuyifisayo@gmail.com>
2022-10-14 22:17:58 -0700

    commited

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@
 
 int main(void)
 {
-int num1, num2
+int num1, num2;
 
-for (num1 = 0; num1 <=98 num1+=)
+for (num1 = 0; num1 <=98; num1+=)
 {
-for (num2= num1 + 1; num2 <= 99; num2++)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
@@ -32,7 +32,7 @@ putchar(',');
 putchar(' ');
 }
 }
-putchar("\n");
+putchar('\n');
 
 return (0);
 }
