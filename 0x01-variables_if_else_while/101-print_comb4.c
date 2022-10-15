70b6285e278e746a3601fac482cd26a237d71d8a
Fisayo <fasuyifisayo@gmail.com>
2022-10-14 22:12:32 -0700

    commited

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@
 
 int main(void)
 {
-int num1, num, num3
+int num1, num, num3;
 
 for (num1 = 0; num1 < 8 num1++)
 {
@@ -23,7 +23,7 @@ for (num3 = num2 + 1; num3 < 10; num3++)
 {
 putchar((num1 % 10) + '0');
 putchar((num2 % 10) + '0');
-putchar((num3 % 10) _ '0');
+putchar((num3 % 10) + '0');
 
 if (num1 == 7 && num2 == 8 && num3 == 9)
 {
