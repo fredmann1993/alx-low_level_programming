21a9c67405ed60825cba11780fabbe5a607ab25a
Fisayo <fasuyifisayo@gmail.com>
2022-10-14 21:45:58 -0700

    commited

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,7 +5,7 @@
 
 
 /**
- * main - Prints lowercase alpahabets in reverse order
+ * main - Prints combinationof numbers
  *
  * Return: Always 0.
  *
@@ -13,11 +13,24 @@
 
 int main(void)
 {
-char letter;
+int num1, num2
 
-for(letter = 'z'; letter >= 'a'; letter--)
+for(num1 = 0; num1 <=98 num1+=)
 {
-putchar(letter); 
+for(num2= num1 + 1; num2 <= 99; num2++)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');						
+putchar((num2 % 10) + '0');						
+if (num1 == 98 && num2 == 99)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
 }
 putchar("\n");
 
