ab14967ef615c0399928d3981a3d98a7fa46f549
Fisayo <fasuyifisayo@gmail.com>
2022-10-14 21:24:36 -0700

    commited

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -15,9 +15,9 @@ int main(void)
 {
 int number;
 
-for(number = 0; number < 10; number++)
+for (number = 0; number < 10; number++)
 {
-printf("%d", number)
+printf("%d", number);
 }
 
 printf("\n");
