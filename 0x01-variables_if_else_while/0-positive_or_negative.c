1dd6ce2f694f756306b1b023e9f5edda7e1aecfe
Fisayo <fasuyifisayo@gmail.com>
2022-10-14 21:01:40 -0700

    commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,7 +23,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive \n", n);
+printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
@@ -31,7 +31,7 @@ printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is 0\n", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
