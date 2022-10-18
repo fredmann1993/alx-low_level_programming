b17c57b00a6cbee7ac8f3f920f548958c5995263
Fisayo <fasuyifisayo@gmail.com>
2022-10-18 16:21:13 -0700

    commited

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+
+/**
+ * add - Adds two values
+ *
+ * @a: The first number to be computed
+ *
+ * @b: The second number to be computed
+ *
+ *
+ *
+ * Return: the result of a and b
+ *
+ */
+
+int add(int a, int b)
+
+{
+
+return (a + b);
+
+}
+
+
