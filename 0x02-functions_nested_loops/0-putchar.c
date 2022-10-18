a487a1d8c45baa3a633c33ab6449a31afd8d1402
Fisayo <fasuyifisayo@gmail.com>
2022-10-18 14:58:39 -0700

    commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,14 +13,17 @@ int main(void)
 
 {
 
-char *put = "putchar";
-while(*put)
-{
-_putchar(*put);
-*put++;
+char *put = "_putchar";
 
 
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
