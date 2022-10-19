298ddd427149e05f0c6c7c5fcded128629e63736
Fisayo <fasuyifisayo@gmail.com>
2022-10-18 17:43:33 -0700

    commited

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,21 +3,14 @@
 
 
 /**
-
  * main - Prints the sum of Even Fibonacci numbers
-
  * less than 4000000.
-
- *
-
  * Return: Nothing!
-
+ *
  */
 
-int main(void)
-  
+int main(void) 
 {
-  
 int i = 0;
 long j = 1, k = 2, sum = k;
 while (k + j < 4000000)
