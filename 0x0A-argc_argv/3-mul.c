a796612a2983f8486acfcc3da2313d6a6e5e141a
Fisayo <fasuyifisayo@gmail.com>
2022-11-04 22:46:41 +0100

    commited

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,11 +16,9 @@ if (argc <= 2)
 printf("Error\n");
 return (1);
 }
-/* atoi convert a string to an int */
 a = atoi(argv[1]);
 b = atoi(argv[2]);
 result = a * b;
 printf("%d\n", result);
 return (0);
 }
-
