6b52cfd5529654b466027d526f121307ffacd086
Fisayo <fasuyifisayo@gmail.com>
2022-11-04 22:40:40 +0100

    commited

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 /**
-* main - prints the no of arguments.
+* main - check the code for Holberton School students.
 * @argv: The character to print
 * @argc: The character to print
 * Return: Always 0.
 */
 int main(int argc, char *argv[])
 {
-int i;
-
-if (argc >= 1)
-{
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-}
+argv = argv;
+printf("%d\n", argc - 1);
 return (0);
 }
