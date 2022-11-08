a14916ea19757d586a0d7e0149e9e815ff479ecb
Fisayo <fasuyifisayo@gmail.com>
2022-11-08 18:32:53 +0100

    commited

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -26,7 +26,8 @@ num++;
 return (num);
 }
 /**
-*  being passed for freeing
+* free_everything - frees the memory
+* @string: pointer values being passed for freeing
 * @i: counter
 */
 void free_everything(char **string, int i)
@@ -48,4 +49,39 @@ char **words, *found_word;
 
 if (str == 0 || *str == 0)
 return (NULL);
-total_words = number
+total_words = number(str);
+if (total_words == 0)
+return (NULL);
+words = malloc((total_words + 1) * sizeof(char *));
+if (words == 0)
+return (NULL);
+for (; *str != '\0' &&  b < total_words;)
+{
+if (*str == ' ')
+str++;
+else
+{
+found_word = str;
+for (; *str != ' ' && *str != '\0';)
+{
+length++;
+str++;
+}
+words[b] = malloc((length + 1) * sizeof(char));
+if (words[b] == 0)
+{
+free_everything(words, b);
+return (NULL);
+}
+while (*found_word != ' ' && *found_word != '\0')
+{
+words[b][c] = *found_word;
+found_word++;
+c++;
+}
+words[b][c] = '\0';
+b++; c = 0; length = 0; str++;
+}
+}
+return (words);
+}
