b84b3a0a9cf6c03e2e97a46e856bb0a6b1360455
Fisayo <fasuyifisayo@gmail.com>
2022-11-18 06:03:10 +0100

    commited

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,48 +1,51 @@
-#include "variadic_functions.h"
+#include "variadic_functions2.h"
 #include <stdio.h>
 #include <stdarg.h>
-
-void op_char(char)
-{
-printf("%c", va_arg(pseudo, char));
-}
-
-void po_int(int)
-{
-printf("%d", va_arg(pseudo, int));
-}
-
-void op_float(float)
-{
-printf("%f", va_arg(pseudo, float));
-}
-
-void op_string(char *)
-{
-printf("%s", va_arg(pseudo, char *));
-}
-
+/**
+* print_all - Entry Point
+* c = char, i = int, f = float, s = char * (if null print (nil))
+* @format: list of arg types
+* Return: 0
+*/
 void print_all(const char * const format, ...)
 {
-va_list pseudo;
-va_start(pseudo, format);
+va_list valist;
+int n = 0, i = 0;
+char *sep = ", ";
+char *str;
 
-op_t ops[] = {
-{"c", op_char},
-{"i", op_int},
-{"f", op_float},
-{"s", op_string},
-{NULL, NULL}
-};
-int i;
+va_start(valist, format);
 
+while (format && format[i])
+i++;
 
-while()
-while()
-if()
-if()
-if()
-
+while (format && format[n])
+{
+if (n  == (i - 1))
+{
+sep = "";
+}
+switch (format[n])
+{
+case 'c':
+printf("%c%s", va_arg(valist, int), sep);
+break;
+case 'i':
+printf("%d%s", va_arg(valist, int), sep);
+break;
+case 'f':
+printf("%f%s", va_arg(valist, double), sep);
+break;
+case 's':
+str = va_arg(valist, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s%s", str, sep);
+break;
+}
+n++;
+}
 printf("\n");
+va_end(valist);
 }
 
